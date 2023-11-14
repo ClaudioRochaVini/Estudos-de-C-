7cc88c193584ee1604ffa8a8fd6f0a5db14e346b
ClaudioRochaVini <84055178+ClaudioRochaVini@users.noreply.github.com>
2023-11-13 22:18:56 -0300

    Add files via upload
    Cadastro de produto, onde no final ele mostrará o valor total de acardo com o preço e a quantidade em estoque.

---
+++ Estoque/Estoque/Produto.cs
@@ -0,0 +1,56 @@
+﻿using System.Globalization;
+
+namespace Estoque {
+    internal class Produto {
+
+        private string _descricao;
+        public int Codigo { get; private set; }
+
+        public double Valor {  get; set;}
+        public int Quantidade {  get; set;}
+
+        public string Descricao {
+            get { return _descricao; }
+            set {
+                if(value != null && value.Length > 1) {
+                    _descricao = value;
+                }
+            }
+        }
+
+        public Produto(int codigo, string descricao, double valor) {
+            Codigo = codigo;
+            Descricao = descricao;
+            Valor = valor;
+        }
+
+        public Produto(int codigo, string descricao, double valor, int quantidade) : this(codigo, descricao, valor) {
+            Entrada(quantidade);
+        }
+
+        public double ValorTotalEmEstoque() {
+            return Valor * Quantidade;
+        }
+        public void Entrada(int entrada) {
+            Quantidade += entrada;
+        }
+
+        public void Saida(int saida) {
+            Quantidade -= saida;
+        }
+
+        public override string ToString() {
+            return "------------------------------------------" +
+                "\nCODIGO..................: "
+                +Codigo
+                +"\nDESCRICAO...............: "
+                +Descricao
+                +"\nVALOR...................: $ "
+                +Valor.ToString("F2", CultureInfo.InvariantCulture)
+                +"\nQNT.....................: "
+                +Quantidade
+                +"\nVALOR TOTAL EM ESTOQUE..: $ "
+                +ValorTotalEmEstoque().ToString("f2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Estoque/Estoque/Program.cs
@@ -0,0 +1,59 @@
+﻿using Estoque;
+using System;
+using System.Globalization;
+
+namespace PrimeiroProjeto {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Produto p;
+
+            Console.WriteLine("==========================================");
+            Console.WriteLine("-------------    CADASTRO   --------------");
+            Console.WriteLine("==========================================");
+            Console.Write("Digite o CODIGO do produto: ");
+            int codigo = int.Parse(Console.ReadLine());
+            Console.Write("Digite a DESCRICAO do produto: ");
+            string descricao =  Console.ReadLine();
+            Console.Write("Digite o VALOR do produto: $ ");
+            double valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Deseja dar ENTRADA no estoque (s/n)? ");
+            char ent = char.Parse(Console.ReadLine());
+
+            if(ent == 's' || ent == 'S') {
+                Console.Write("QUANTIDADE de produtos para entrada: ");
+                int qnt = int.Parse(Console.ReadLine());
+                p = new Produto(codigo,descricao, valor, qnt);
+            }
+            else {
+                p = new Produto(codigo, descricao, valor);
+            }
+
+            Console.WriteLine(p);
+
+            Console.WriteLine();
+            Console.Write("Dar ENTRADA no estoque: ");
+            int quantidade = int.Parse(Console.ReadLine());
+            p.Entrada(quantidade);
+            Console.WriteLine(p);
+
+            Console.WriteLine();
+            Console.Write("Dar SAIDA no estoque: ");
+            quantidade = int.Parse(Console.ReadLine());
+            p.Saida(quantidade);
+            Console.WriteLine(p);
+
+            Console.WriteLine();
+            Console.Write("Atualizar DESCRICAO (s/n)? ");
+            char desc = char.Parse(Console.ReadLine());
+            if (desc == 's' || desc == 'S') {
+                Console.Write("Nova DESCRICAO: ");
+                descricao = Console.ReadLine();
+                p.Descricao = descricao;
+                Console.WriteLine(p);
+            }
+
+        }
+    }
+}
